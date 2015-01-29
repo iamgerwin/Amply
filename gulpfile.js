d46b0e0a447b07232901ef4e8dcd31f99638d7b4
iamgerwin <iamgerwin@aim.com>
2015-01-29 16:41:37 +0800

    Update Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,13 @@ var autoprefix = require('gulp-autoprefixer');
 var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
+var livereload = require('gulp-livereload');
 
-var sassDir = 'app/assets/sass';
+
+var sassDir = 'app/assets/sass/*.sass';
 var targetCssDir = 'public/assets/css/*.css';
 
-var coffeeDir = 'app/assets/coffee';
+var coffeeDir = 'app/assets/coffee/*.coffee';
 var targetJsDir = 'public/assets/js/*.js';
 
 var distDir = 'public/dist';
@@ -29,11 +31,22 @@ gulp.task('min-css', function()
 		.pipe(gulp.dest(distDir));
 });
 
+gulp.task('sass', function()
+{
+	return sass(sassDir)
+			.pipe(gulp.dest(targetCssDir))
+			.pipe(livereload());
+});
+
 gulp.task('watch', function()
 {
+	var server = livereload();
+
 	gulp.watch(targetJsDir,['min-js']);
+	gulp.watch(targetCssDir,['min-css']);
+	gulp.watch(sassDir,['sass']);
 });
 
 
 
-gulp.task('default', ['min-js','min-css']);
+gulp.task('default', ['sass','min-js','min-css','watch']);
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,1 @@
 var js = 'gerwin';
-
-function getPrice() {
-    return 100;
-}
-
-function getName() {
-	return "Gerwin";
-}
